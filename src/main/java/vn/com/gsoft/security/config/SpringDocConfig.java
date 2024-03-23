341c86d3debb38120bc989fb17b9fa6ba1ca37ba
giangnt <giangnt@tecapro.com.vn>
2024-03-23 23:19:19 +0700

    update

---
+++ src/main/java/vn/com/gsoft/security/config/SpringDocConfig.java
@@ -0,0 +1,8 @@
+package vn.com.gsoft.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+}
