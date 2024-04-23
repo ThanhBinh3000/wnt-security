bbb6f0e6aa8927f31e9d25f3b89ed01e6efe333d
giangnt <giangnt@tecapro.com.vn>
2024-04-23 09:19:57 +0700

    update

--- src/main/java/vn/com/gsoft/security/repository/SettingsRepository.java
+++ src/main/java/vn/com/gsoft/security/repository/SettingsRepository.java
@@ -1,5 +1,6 @@
 package vn.com.gsoft.security.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import vn.com.gsoft.security.entity.Settings;
@@ -8,6 +9,8 @@ import java.util.List;
 
 @Repository
 public interface SettingsRepository extends CrudRepository<Settings, Long> {
-
+    @Query(value = "SELECT * " +
+            "FROM Settings c " +
+            "WHERE c.MaNhaThuoc = ?1", nativeQuery = true)
     List<Settings> findByMaNhaThuoc(String maNhaThuoc);
 }
